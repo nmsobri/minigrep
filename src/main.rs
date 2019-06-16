0da9d53b6a2832b56148ccd467e96d9b0415201c
slier81 <slier81@gmail.com>
2019-06-16 22:40:40 +0800

    First init 20190616

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use std::env;
+use std::fs::File;
+use std::io::Read;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
+
+    let mut file = File::open(filename).expect(format!("Cant find file {}", filename).as_str());
+    let mut content = String::new();
+
+    file.read_to_string(&mut content).expect(format!("Cant read file {}", filename).as_str());
+
+    println!("Content of {} is \n{}", filename, content);
+}
+
+fn parse_args() -> (String, String) {
+    let args: Vec<String> = env::args().collect();
+    return (args[1].clone(), args[2].clone());
+}

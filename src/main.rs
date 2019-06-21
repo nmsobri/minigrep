5ca314d59474a76063d418cb041556deeef11d24
slier81 <slier81@gmail.com>
2019-06-21 22:38:21 +0800

    Properly redirect error to stderr 20190621

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing args: {}", err);
+        eprintln!("Problem parsing args: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = run(config) {
-        println!("Error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
